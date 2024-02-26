92c89f350551df19723ca25f3c4547fe8ea7ce75
강종찬 <jckang@jckang-MacBook-Air.local>
2024-02-26 22:47:38 +0900

    init

---
+++ module-api/build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val projectGroup: String by project
+val applicationVersion: String by project
+
+group = projectGroup
+version = applicationVersion
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.jar {
+    enabled = true
+}
